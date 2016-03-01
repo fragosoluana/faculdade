a33687ab18cf04f9d330bddab87f54aa714f952b
Luana Fragoso <fragosoluana@github.com>
2016-03-01 00:28:16 -0300

    [COMP GRAFICA] - Criacao da classe Vector para armazenar os valores de x, y e z do espaco - Criacao da excecao VetorNuloException para ser lancada quando um vetor nulo e detectado - Criacao da classe Operation responsavel por todas as operacoes de vetores, entre vetores e pontos

---
+++ src/com/eic/cefet/cg/vector/entity/Vector.java
@@ -0,0 +1,51 @@
+package com.eic.cefet.cg.vector.entity;
+
+public class Vector {
+	
+	private double x;
+	private double y;
+	private double z;
+	
+	public Vector(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector other = (Vector) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		if (Double.doubleToLongBits(z) != Double.doubleToLongBits(other.z))
+			return false;
+		return true;
+	}
+}
---
+++ src/com/eic/cefet/cg/vector/exception/VetorNuloException.java
@@ -0,0 +1,10 @@
+package com.eic.cefet.cg.vector.exception;
+
+public class VetorNuloException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
